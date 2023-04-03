7c14d0e550ab6e56802c94afc595eb1a8bb40f3d
megha <meghamini40@gmail.com>
2023-04-03 23:45:26 +0530

    added world map and area

--- pages/Continents/index.js
+++ pages/Continents/index.js
@@ -4,6 +4,7 @@
 //   console.log(data, "data");
 
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export const continents = [
     "Europe",
@@ -15,17 +16,25 @@ export const continents = [
 ];
 
 const Continents = () => {
+    const router = useRouter();
+    const continent = router.query.name;
     return (
         <>
             <div>continents</div>
-            {continents.map((region, i) => {
+            <img src="https://img.freepik.com/free-photo/global-globalization-world-map-environmental-concservation-concept_53876-120363.jpg?w=1380&t=st=1680545103~exp=1680545703~hmac=83dc36ce8309abc7ac82169f7d5bcb446789a602e530842833f7400e2575b497" width={1300} height={250} alt="worldmap" useMap="#worldmap" />
+            {/* {continents.map((region, i) => {
                 i++;
-                return (
-                    <Link href={`/Continents/${region}`} key={i}>
+                return ( */}
+            <map name="worldmap">
+                <area shape="rect" coords="0,0,82,126" alt="continents" href={`/Continents/${continents[0]}`} className="bg-green-800 hover:bg-blue-600" />
+                <area shape="rect" coords="34,44,270,350" alt="continents" href={`/Continents/${continents[1]}`} className="bg-green-800 hover:bg-blue-600" />
+                <area shape="rect" coords="290,172,333,250" alt="continents" href={`/Continents/${continents[2]}`} className="bg-green-800 hover:bg-blue-600" />
+            </map>
+            {/* <Link href={`/Continents/${region}`} key={i}>
                         <h3 className="text-3xl">{region}</h3>
                     </Link>
                 );
-            })}
+            })} */}
         </>
     );
 };
