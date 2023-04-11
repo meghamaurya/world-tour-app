63c6d7420f3af29c21dfbdfbc67decc4f2ff4724
megha <meghamini40@gmail.com>
2023-04-11 22:58:15 +0530

    created search fields

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,12 @@
 
 const Search = () => {
+    const handleChange = (e) => {
+        console.log(e.target.value, "search")
+    }
     return (
-        <div>
-            search
+        <div className="flex gap-1">
+            <input className="border border-blue-500 rounded-md" onChange={handleChange} />
+            <button className="bg-blue-400 px-2 font-bold rounded-md">search</button>
         </div>
     )
 }
