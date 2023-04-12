7c889cc8af873f7d8d33c2e68503c74ac088c05d
megha <meghamini40@gmail.com>
2023-04-12 22:59:20 +0530

    build navbar

--- pages/nav.js
+++ pages/nav.js
@@ -3,7 +3,7 @@ import Search from "./search";
 const Navbar = ({ children }) => {
     return (
         <div className="flex flex-col">
-            <div className="grid grid-rows-3 justify-evenly">
+            <div className="grid grid-cols-3 justify-evenly">
                 <h2 className="col-span-1">World Tour</h2>
                 <Search />
             </div>
